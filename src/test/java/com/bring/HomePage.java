49529187d29ecc4fb5942a73711925fa360a3bab
Sergio Riveros <ariveros142@gmail.com>
2021-01-30 23:38:27 -0500

    Update HomePage.java

--- src/test/java/com/bring/HomePage.java
+++ src/test/java/com/bring/HomePage.java
@@ -4,7 +4,10 @@ import io.appium.java_client.MobileElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+// This class represents the initial page when you have to click the add contact button
 public class HomePage extends base{
+    
+    // Identification of the elements
     MobileElement buttonAddContact = (MobileElement) driver.findElementById("com.android.contacts:id/add_contact_button");
 
     public void clickAddContact(){
